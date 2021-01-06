6bbf05006268d2fa23616ae4db1f6068663d130d
phihuynh94 <phihuynh94@gmail.com>
2021-01-06 12:47:56 -0700

    add redux to home screen

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,29 +1,20 @@
-import React, { useEffect, useState } from "react";
-import axios from "axios";
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
 
 import Product from "../components/Product";
 import LoadingBox from "../components/LoadingBox";
 import MessageBox from "../components/MessageBox";
 
-const HomeScreen = () => {
-  const [products, setProducts] = useState([]);
-  const [loading, setLoading] = useState(false);
-  const [error, setError] = useState(false);
+import { listProducts } from "../actions/productActions";
 
-  const fetchData = async () => {
-    try {
-      setLoading(true);
-      const { data } = await axios.get("/api/products");
-      setLoading(false);
-      setProducts(data);
-    } catch (err) {
-      setError(err.message);
-      setLoading(false);
-    }
-  };
+const HomeScreen = () => {
+  const dispatch = useDispatch();
+
+  const productList = useSelector((state) => state.productList);
+  const { loading, error, products } = productList;
 
   useEffect(() => {
-    fetchData();
+    dispatch(listProducts());
   }, []);
 
   return (
